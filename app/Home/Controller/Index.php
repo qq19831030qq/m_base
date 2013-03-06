b9de3355a4c30a00d185a16346c71e2e64a2ffe0
charles shen <qq19831030qq@gmail.com>
2013-03-07 01:06:50 +0800

    1

---
+++ app/Home/Controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+class Home_Controller_Index extends controller{
+    public function init(){
+        parent::init();
+    }
+
+    public function actionIndex(){
+        $model_test = MONK::getSingleton('Home_Model_Test');
+        $t = $model_test->M('select',array('test1','test2'),array('test1.name'=>'t111'),true);
+        dump($t);
+        $this->render();
+    }
+}
